50d9808cc7630a2f777d6174c2cf203eced7735f
Josh Holladay <jholladay@nuskin.com>
2021-04-19 18:38:32 -0600

    Update: positioned header and logo

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <MenuHeader />
-      <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
 }
--- src/components/MenuHeader.tsx
+++ src/components/MenuHeader.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import header from '../assets/header-background.png';
+import logo from '../assets/Calico-logo-icon.png';
+import '../styles/MenuHeader.css';
 
 function MenuHeader() {
     return (
-      <div className="Header">
-        <img src={header} className="header-background" alt="header background" />
+      <div className="MenuHeader">
+        <div className="wrapper">
+          <img src={logo} className="calico-logo-icon" alt="calico logo" />
+        </div>
       </div>
     );
   }
-
-export default MenuHeader;
+  
+  export default MenuHeader;
+  
